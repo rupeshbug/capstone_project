fed1f0b3ed92b2ff8454ffb6236ed097a0ab6397
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2024-08-10 11:05:07 +0545

    features section done

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { MoveRight } from "lucide-react";
 
 const FeatureCard = ({ icon: Icon, title, description }) => {
   return (
-    <div className="bg-gray-100 shadow-xl rounded-lg p-6 flex flex-col items-center text-center transition-transform transform cursor-pointer hover:scale-105">
-      <div className="flex gap-1 items-center">
-        <span>
-          <Icon className="text-teal-600 mb-4" size={32} />
-        </span>
-        <h3 className="text-xl text-gray-800 font-semibold mb-2">{title}</h3>
-      </div>
+    <div className="bg-gray-100 shadow-xl rounded-lg px-6 py-8 flex flex-col items-center text-center transition-transform transform cursor-pointer hover:scale-105">
+      <span className="text-left">
+        <Icon className="text-teal-600 mb-4" size={40} />
+      </span>
+      <h3 className="text-xl text-gray-800 font-semibold mb-2">{title}</h3>
       <p className="text-gray-700">{description}</p>
+      <div className="mt-6 flex items-center gap-1 font-semibold opacity-80">
+        <p className="text-red-500">Learn More</p>
+        <MoveRight size={20} className="text-red-500" />
+      </div>
     </div>
   );
 };
