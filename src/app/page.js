5572bd28ff18423864b56d8d3a97d6e68b3f07c5
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2024-09-20 18:21:44 +0545

    build error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import Features from "../components/Features";
 import HowItWorks from "../components/HowItWorks";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
