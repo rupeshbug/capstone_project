e99f5d38ee7118afa2c510cd3350404987b54309
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2024-09-24 13:44:37 +0545

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
           <ChartNoAxesCombined size={32} color="#0d9488" />
         </span>
       </div>
-      <div className="hidden xl:flex w-[40%] space-x-24 text-[18px]">
+      <div className="hidden xl:flex w-[40%] space-x-32 text-[18px]">
         <Link className="hover:text-teal-600" href="/">
           Home
         </Link>
@@ -50,17 +50,10 @@ function Navbar() {
           Features
         </Link>
       </div>
-      <div className="hidden lg:flex w-[20%] space-x-16 text-[18px]">
-        <Link
-          className="border-2 py-2 px-6 border-teal-500 rounded-2xl hover:bg-teal-500 hover:text-white"
-          href="/"
-        >
-          Login
-        </Link>
-        <div className="flex gap-2 items-center border-2 py-2 px-6 border-teal-500 rounded-2xl hover:bg-teal-500 hover:text-white">
-          <Rocket size={20} color="#933a34" />
-          <Link href="/">Signup</Link>
-        </div>
+
+      <div className="hidden lg:flex gap-2 items-center border-2 py-2 px-6 border-teal-500 rounded-2xl hover:bg-teal-500 hover:text-white">
+        <Rocket size={20} color="#933a34" />
+        <Link href="/">Try It Now</Link>
       </div>
       <button className="xl:hidden text-gray-800" onClick={toggleMenu}>
         <Menu size={32} />
@@ -70,7 +63,7 @@ function Navbar() {
           <div className="fixed inset-0 bg-gray-900 opacity-30 z-40" />
           <div
             id="mobile-menu"
-            className="fixed top-0 right-0 h-full w-3/4 lg:w-2/4 bg-gray-800 p-6 z-50 opacity-95 transform transition-transform duration-300 ease-in-out"
+            className="fixed top-0 right-0 h-full w-3/4 lg:w-2/4 bg-gray-800 p-6 z-50 opacity-95 transition-transform duration-600 ease-in"
             style={{
               transform: isMenuOpen ? "translateX(0)" : "translateX(100%)",
             }}
@@ -82,6 +75,14 @@ function Navbar() {
               <X size={32} />
             </button>
             <div className="flex flex-col mt-16 space-y-8 text-center">
+              <div className="flex gap-1 cursor-pointer items-center justify-center mb-2">
+                <Link href="/">
+                  <h1 className="text-white text-2xl">Intextify</h1>
+                </Link>
+                <span>
+                  <ChartNoAxesCombined size={32} color="#0d9488" />
+                </span>
+              </div>
               <Link href="/" className="text-white text-xl" onClick={closeMenu}>
                 Home
               </Link>
@@ -99,13 +100,6 @@ function Navbar() {
               >
                 Features
               </Link>
-              <Link
-                href="/"
-                className="border-2 border-teal-500 text-xl w-[80%] mx-auto py-2 px-6 rounded-2xl text-white hover:bg-teal-500 hover:text-white"
-                onClick={closeMenu}
-              >
-                Login
-              </Link>
               <div className="flex gap-2 items-center justify-center mx-auto w-[80%] border-2 py-2 px-6 border-teal-500 rounded-2xl text-teal-500 hover:bg-teal-500 hover:text-white">
                 <Rocket size={20} color="#933a34" />
                 <Link
@@ -113,7 +107,7 @@ function Navbar() {
                   className="text-white text-xl"
                   onClick={closeMenu}
                 >
-                  Signup
+                  Try It Now
                 </Link>
               </div>
             </div>
